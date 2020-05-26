6c5cde521a7fd736d537c047379971a5cf0473cc
vijay-ss <54510959+vijay-ss@users.noreply.github.com>
2020-05-26 01:41:12 -0400

    remove border from news feed

--- app.py
+++ app.py
@@ -390,7 +390,7 @@ news_feed = html.Div([
              children=[
                  html.Iframe(
                      src='http://www.rssdog.com/index.php?url=http%3A%2F%2Fwww.cbc.ca%2Fcmlink%2Frss-world&mode=html&showonly=&maxitems=20&showdescs=1&desctrim=0&descmax=0&tabwidth=100%25&showdate=1&linktarget=_blank&fullhtml=1&bordercol=transparent&headbgcol=transparent&headtxtcol=%23ffffff&titlebgcol=transparent&titletxtcol=%23ffffff&itembgcol=transparent&itemtxtcol=%23ffffff&ctl=0',
-                     style={'width':'107.7%', 'height':'42rem', 'display':'inline-block'}
+                     style={'width':'107.7%', 'height':'42rem', 'display':'inline-block', 'border':'none'}
                      #height=600,
                      #width=1845
                              )
