be59b47e9d14e0ad393d4d79ed8f18b8828f3b0b
vijay-ss <54510959+vijay-ss@users.noreply.github.com>
2020-05-30 20:06:23 -0400

    update landing page to layout 1

--- app.py
+++ app.py
@@ -415,7 +415,7 @@ def display_page(pathname):
     elif pathname == '/page-4':
         return number_plates, page_4_layout
     else:
-        return number_plates, news_feed
+        return number_plates, page_1_layout
 
 
 if __name__ == '__main__':
