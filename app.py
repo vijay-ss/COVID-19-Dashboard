7cea1b6828c179c22d3170860c0dfdfc7a1ab101
vijay-ss <54510959+vijay-ss@users.noreply.github.com>
2020-05-31 13:36:14 -0400

    update main app after etl separation
    1) call the etl file
    2) import the main csv files into the data frame
    3) update data frame reference names throughout the script

--- app.py
+++ app.py
@@ -13,88 +13,27 @@ from datetime import date, timedelta
 import twitter_web_scrape
 df_tweets = pd.read_csv(r'data/covid_tweets.csv')
 
-### Data Pre-processing ###
-# todo create a separate etl file to do the pre-processing and call it
+import ETL
+countries_df = pd.read_csv(r'data/countries.csv')
+global_daily_count = pd.read_csv(r'data/global_daily_count.csv')
+global_melt = pd.read_csv(r'data/global_melt.csv')
+canada_df = pd.read_csv(r'data/canada.csv')
+canada_province = pd.read_csv(r'data/canada_by_province.csv')
 
-url_1 = 'https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv'
-url_2 = 'https://raw.githubusercontent.com/datasets/covid-19/master/data/countries-aggregated.csv'
-url_3 = 'https://raw.githubusercontent.com/datasets/covid-19/master/data/key-countries-pivoted.csv'
-url_4 = 'https://raw.githubusercontent.com/datasets/covid-19/master/data/worldwide-aggregated.csv'
-time_series = pd.read_csv(url_1, index_col=0,parse_dates=[0]).reset_index()
-countries = pd.read_csv(url_2, index_col=0,parse_dates=[0]).reset_index()
-countries_pv = pd.read_csv(url_3, index_col=0,parse_dates=[0]).reset_index()
-ww_agg = pd.read_csv(url_4, index_col=0,parse_dates=[0]).reset_index()
+
+### Data Pre-processing ###
 
 # convert Date object to Datetime
-countries['Date'] = pd.to_datetime(countries['Date'])
-countries_pv['Date'] = pd.to_datetime(countries_pv['Date'])
-time_series['Date'] = pd.to_datetime(time_series['Date'])
-ww_agg['Date'] = pd.to_datetime(ww_agg['Date'])
-
-# A df of all countries, incl co-ordinates for map
-# A cleaner country df, without province/state but includes lat and long for mapping purposes
-countries_df = time_series.copy()
-countries_df.rename(columns={"Country/Region": "Country"}, inplace=True)
-countries_df.fillna(0, inplace=True)
-countries_df.drop(['Province/State'], axis=1, inplace=True)
-countries_df['Active'] = countries_df['Confirmed'] - countries_df['Recovered']
+countries_df['Date'] = pd.to_datetime(countries_df['Date'])
+global_daily_count['Date'] = pd.to_datetime(global_daily_count['Date'])
+global_melt['Date'] = pd.to_datetime(global_melt['Date'])
+canada_df['Date'] = pd.to_datetime(canada_df['Date'])
+canada_province['Date'] = pd.to_datetime(canada_province['Date'])
 
 
-countries_df = countries_df.groupby(['Date', 'Country'], as_index=False).agg({'Lat': 'mean',
-                                                                       'Long': 'mean',
-                                                                       'Confirmed': 'sum',
-                                                                       'Deaths': 'sum',
-                                                                       'Recovered': 'sum',
-                                                                       'Active':'sum'
-                                                                     })
-
-# update country centroid which are spread due to provinces/colonies
-countries_df.loc[countries_df['Country'] == 'US', 'Lat'] = 39.810489
-countries_df.loc[countries_df['Country'] == 'US', 'Long'] = -98.555759
-
-countries_df.loc[countries_df['Country'] == 'France', 'Lat'] = 46.2276
-countries_df.loc[countries_df['Country'] == 'France', 'Long'] = 2.2137
-
-countries_df.loc[countries_df['Country'] == 'United Kingdom', 'Lat'] = 55.3781
-countries_df.loc[countries_df['Country'] == 'United Kingdom', 'Long'] = -3.4360
-
-countries_df.loc[countries_df['Country'] == 'Denmark', 'Lat'] = 56.2639
-countries_df.loc[countries_df['Country'] == 'Denmark', 'Long'] = 9.5018
-
-countries_df.loc[countries_df['Country'] == 'Netherlands', 'Lat'] = 52.1326
-countries_df.loc[countries_df['Country'] == 'Netherlands', 'Long'] = 5.2913
-
-countries_df.loc[countries_df['Country'] == 'Canada', 'Lat'] = 59.050000
-countries_df.loc[countries_df['Country'] == 'Canada', 'Long'] = -112.833333
-
-countries_df['Active'] = countries_df['Confirmed'] - countries_df['Recovered'] - countries_df['Deaths']
-
-# Description for map
-countries_df['Description'] = countries_df['Country'] + '<br>' \
-+ 'Confirmed: ' + countries_df['Confirmed'].astype(int).map('{:,.0f}'.format) + '<br>' \
-+ 'Deaths: ' + countries_df['Deaths'].astype(int).map('{:,.0f}'.format) + '<br>' \
-+ 'Recovered: ' + countries_df['Recovered'].astype(int).map('{:,.0f}'.format) + '<br>' \
-+ 'Active: ' + countries_df['Active'].astype(int).map('{:,.0f}'.format)
-#countries_df = countries_df.assign(logCumConf = np.where(countries_df['Confirmed'] > 0, np.log(countries_df['Confirmed']) / np.log(2), 0))
-
-# Total daily counts
-national_daily_count = countries_df.groupby(['Date','Country'])['Confirmed','Deaths','Recovered','Active'].sum().reset_index()
-global_daily_count = countries_df.groupby(['Date'])['Confirmed','Deaths','Recovered','Active'].sum().reset_index()
-global_melt = global_daily_count.melt(id_vars=['Date'],
-                                      value_vars=['Recovered','Deaths','Active','Confirmed'],
-                                      var_name='Type',value_name='Count'
-                                      )
-
-# Data for Canada - by province
-canada_df = time_series.copy()
-canada_df = canada_df[canada_df['Country/Region'] == 'Canada'].reset_index(drop=True)
-canada_df.rename(columns={"Country/Region": "Country"}, inplace=True)
-canada_df.fillna(0, inplace=True)
-canada_df['Active'] = canada_df['Confirmed'] - canada_df['Recovered'] - canada_df['Deaths']
 
 # Canada day over day
-p = canada_df.groupby(['Date','Province/State'])['Confirmed'].sum().reset_index()
-p['Diff'] = p.groupby('Province/State')['Confirmed'].diff(periods=1)
+canada_province['Diff'] = canada_province.groupby('Province/State')['Confirmed'].diff(periods=1)
 
 '''PLOTS'''
 
@@ -157,7 +96,6 @@ fig_can = px.line(x=canada_df['Date'],
         render_mode="svg",
         template='plotly_dark'
 )
-
 # Add labels
 fig_can.update_layout(
     title = "Timeline of Confirmed Cases - by Province",
@@ -167,15 +105,14 @@ fig_can.update_layout(
 )
 
 #Ontario day over day
-fig_dod = px.line(x=p['Date'],
-        y=p["Diff"],
-        color=p['Province/State'],
-        hover_name=p['Province/State'],
+fig_dod = px.line(x=canada_province['Date'],
+        y=canada_province["Diff"],
+        color=canada_province['Province/State'],
+        hover_name=canada_province['Province/State'],
         line_shape="linear",
         render_mode="svg",
         template='plotly_dark'
 )
-
 # Add labels
 fig_dod.update_layout(
     title = "Day over day - by Province",
@@ -232,7 +169,7 @@ app.layout = html.Div(children=[
     html.H1(children='Covid-19 Interactive Tracker',
             style={'textAlign':'center'}),
 
-    html.Div(children='Data last updated: ' + time_series['Date'].max().strftime('%Y-%m-%d') +
+    html.Div(children='Data last updated: ' + global_daily_count['Date'].max().strftime('%Y-%m-%d') +
                       ' (Maintained by John Hopkins University)',
              style={'textAlign':'center'}),
 
@@ -360,14 +297,14 @@ page_1_layout = html.Div([
                        ])
 ])
 
-df = countries_df[countries_df['Date'] == countries_df['Date'].max()].groupby(['Country'])['Confirmed','Deaths','Recovered','Active'].sum().sort_values('Confirmed',ascending=False).reset_index()
-df['Rank'] = df.index + 1
-df = df[['Rank','Country','Confirmed','Deaths','Recovered','Active']]
-df[['Confirmed','Deaths','Recovered','Active']] = df[['Confirmed','Deaths','Recovered','Active']].astype(int).applymap('{:,}'.format)
+df_ranking = countries_df[countries_df['Date'] == countries_df['Date'].max()].groupby(['Country'])['Confirmed','Deaths','Recovered','Active'].sum().sort_values('Confirmed',ascending=False).reset_index()
+df_ranking['Rank'] = df_ranking.index + 1
+df_ranking = df_ranking[['Rank','Country','Confirmed','Deaths','Recovered','Active']]
+df_ranking[['Confirmed','Deaths','Recovered','Active']] = df_ranking[['Confirmed','Deaths','Recovered','Active']].astype(int).applymap('{:,}'.format)
 
 page_3_layout = html.Div([
     html.Div(id='page-3'),
-    html.Div(dbc.Table.from_dataframe(df.head(20),striped=True, bordered=True, hover=True))
+    html.Div(dbc.Table.from_dataframe(df_ranking.head(20),striped=True, bordered=True, hover=True))
 ], style={'marginLeft': '1.5%', 'marginRight': '1.5%', 'marginBottom': '.5%', 'marginTop': '.5%'})
 
 page_4_layout = html.Div([
